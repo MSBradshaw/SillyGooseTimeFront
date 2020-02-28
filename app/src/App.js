c0e113fed3bcc5249d8087c18073dddb919da1fb
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-02-28 15:26:11 -0700

    created basic front end for to eventually use to test sending information from the front end to the backend

---
+++ app/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './big-goose.png';
+import './App.css';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: 'Default Message'
+        }
+    }
+    render(){
+        return(
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        It's Silly Goose Time!
+                    </p>
+                    <form>
+                        <label for="message">Secret Message:</label>
+                        <input type="text" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)}></input>
+                        <button onClick={this.sendMessage.bind(this)}>Send Message</button>
+                    </form>
+                </header>
+            </div>
+        )
+    }
+    onMessageChange(event){
+        this.setState({message: event.target.value})
+    }
+    sendMessage(event){
+        event.preventDefault()
+        console.log(this.state.message)
+    }
+}
+
+export default App;
