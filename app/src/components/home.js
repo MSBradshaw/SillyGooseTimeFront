d9aaa186d52c8ee6d08e37abad9f5c415efc8d27
Vinitha Gadiraju <vinitha.gadiraju@colorado.edu>
2020-04-07 17:16:31 -0600

    dark mode added

--- app/src/components/home.js
+++ app/src/components/home.js
@@ -11,26 +11,17 @@ class Home extends React.Component {
             message: 'Default Message'
         }
     }
+
     render(){
         return(
             <div class='homepage'>
-                <html>
-                <body>
+
                 <p>A Silly Goose Time is meant to describe a fun time spent with good people. Examples of A Silly Goose Time are: playing pick-up soccer, going to see a new movie, trying a new restaurant, or just going for a stroll. A Silly Goose Time is not stressful. A Silly Goose Time is inclusive. A Silly Goose Time is safe, relaxed, fun, and-well-SILLY!</p>
-                <button onclick="myFunction()">Toggle dark mode</button>
-                <script>
-                function myFunction() {
-                   var element = document.body;
-                   element.classList.toggle("dark-mode");
-                }
-                </script>
-                </body>
-                </html>
-        
-                 <div class='homepage__img_wrapper'>
+     
+                <div class='homepage__img_wrapper'>
                     <img src={sillygooseimg} alt=""/>
                 </div>
-            </div>
+    		</div>
         )
     }
 }
