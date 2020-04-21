88dfb86b644eb98eafd47219cd6cb65dbf038341
Tzu-Chi Yen <tzuchi.yen@colorado.edu>
2020-04-21 17:07:29 -0600

    updated npm packages

--- app/deploy.sh
+++ app/deploy.sh
@@ -2,4 +2,4 @@
 
 npm run-script build
 aws s3 cp ./build s3://silly-goose-time-front --recursive --acl public-read --profile sillygoose
-aws cloudfront create-invalidation --distribution-id E3ATUT0WZ1Y4M6 --paths "/*"
+aws cloudfront create-invalidation --distribution-id E3ATUT0WZ1Y4M6 --paths "/*" --profile sillygoose
